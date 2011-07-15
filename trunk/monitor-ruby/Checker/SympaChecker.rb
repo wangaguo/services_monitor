97c5dd08f47d49e7856642b3b82f2837cd19203b
wangaguo <wangaguo@2173ef0e-03d7-dd11-8004-000bcdefe56b>
2011-07-15 07:42:13 +0000

    For r17 lost the SympaChecker.

---
+++ trunk/monitor-ruby/Checker/SympaChecker.rb
@@ -0,0 +1,12 @@
+class SympaChecker 
+  def checker(conf, log)
+      if Time.now.strftime("%H:%M") == conf["ok_mail_time"]
+        send_email(conf["email_from"], conf["sympa_monitor_mail_to"], "Monitor Sympa - #{Time.now.strftime("%Y/%m/%d")}", "Sympa is ok.")
+      end
+      return "" 
+    rescue
+      puts "#{$!}"
+      return "#{$!}"
+    end
+  end
+end
