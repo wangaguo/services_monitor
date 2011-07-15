b235db926644b83adc8ecfa61eb0645fb6997c28
wangaguo <wangaguo@gmail.com>
2011-07-15 07:47:36 +0000

    Lost begin for rescue.

--- monitor-ruby/Checker/SympaChecker.rb
+++ monitor-ruby/Checker/SympaChecker.rb
@@ -1,5 +1,6 @@
 class SympaChecker 
   def checker(conf, log)
+    begin
       if Time.now.strftime("%H:%M") == conf["ok_mail_time"]
         send_email(conf["email_from"], conf["sympa_monitor_mail_to"], "Monitor Sympa - #{Time.now.strftime("%Y/%m/%d")}", "Sympa is ok.")
       end
